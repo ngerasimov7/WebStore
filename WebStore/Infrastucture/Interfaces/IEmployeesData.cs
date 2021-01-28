0330a88e9432122ad63e29ebcb8fc022b1768e37
ngerasimov7 <nikola20@mail.ru>
2021-01-28 23:14:45 +0300

    Создание интерфейса IEmployeesData

---
+++ WebStore/Infrastucture/Interfaces/IEmployeesData.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using WebStore.Models;
+
+namespace WebStore.Infrastucture.Interfaces
+{
+    public interface IEmployeesData
+    {
+        IEnumerable<Employee> Get();
+        Employee Get(int id);
+        int Add(Employee employee);
+        void Update(Employee employee);
+        bool Delete(int id);
+    }
+}
